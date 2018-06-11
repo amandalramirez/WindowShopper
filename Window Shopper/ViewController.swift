3cd8dd1b9688ad56a66966e59763ab9b79b58b6c
Amanda Ramirez <ohh.soo.cutee@hotmail.com>
2018-06-11 11:38:16 -0400

    Initial Commit

---
+++ Window Shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window Shopper
+//
+//  Created by Amanda Ramirez on 6/11/18.
+//  Copyright © 2018 Amanda Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
